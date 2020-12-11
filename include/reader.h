0cfb2a1091673090ac96325e2a35421e667b2bce
Devessier <baptiste@devessier.fr>
2020-12-11 11:51:11 +0100

    feat: create a reader that can read from a file or from a buffer

--- include/reader.h
+++ include/reader.h
@@ -6,13 +6,52 @@
 /*   By: bdevessi <baptiste@devessier.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/08 02:08:41 by bdevessi          #+#    #+#             */
-/*   Updated: 2020/12/08 03:29:06 by bdevessi         ###   ########.fr       */
+/*   Updated: 2020/12/11 11:39:42 by bdevessi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef READER_H
 # define READER_H
+# include <unistd.h>
+# define BUFF_SIZE 4096
 
-int		lol(void);
+enum				e_reader_type
+{
+	READER_TYPE_FD = 1,
+	READER_TYPE_BUFFER
+};
+
+typedef struct		s_reader_fd_context
+{
+	int		fd;
+}					t_reader_fd_context;
+
+typedef struct		s_reader_buffer_context
+{
+	char	*data;
+	size_t	data_length;
+	size_t	cursor;
+}					t_reader_buffer_context;
+
+typedef union		u_reader_context
+{
+	t_reader_fd_context			fd;
+	t_reader_buffer_context		buffer;
+}					t_reader_context;
+
+typedef struct		s_reader
+{
+	char				buffer[BUFF_SIZE];
+	size_t				index;
+	size_t				length;
+	ssize_t				(*fill)(struct s_reader *reader);
+
+	enum e_reader_type	type;
+	t_reader_context	ctx;
+}					t_reader;
+
+t_reader			create_reader_fd(int fd);
+t_reader			create_reader_buffer(char *buffer, size_t buffer_length);
+ssize_t				reader_read(t_reader *reader, char *dest, size_t length);
 
 #endif
--- srcs/reader.c
+++ srcs/reader.c
@@ -6,11 +6,83 @@
 /*   By: bdevessi <baptiste@devessier.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/08 02:03:25 by bdevessi          #+#    #+#             */
-/*   Updated: 2020/12/08 02:03:41 by bdevessi         ###   ########.fr       */
+/*   Updated: 2020/12/11 11:40:25 by bdevessi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		lol(void)
+#include <unistd.h>
+#include "libft.h"
+#include "reader.h"
+
+static ssize_t		reader_read_fd(t_reader *reader)
 {
-	return 0;
+	ssize_t		nbytes;
+
+	if ((nbytes = read(reader->ctx.fd.fd, reader->buffer, BUFF_SIZE)) == -1)
+		return (-1);
+	reader->index = 0;
+	reader->length = nbytes;
+	return (nbytes);
+}
+
+static ssize_t		reader_read_buffer(t_reader *reader)
+{
+	size_t	remaining;
+	size_t	to_copy;
+
+	remaining = reader->ctx.buffer.data_length - reader->ctx.buffer.cursor;
+	to_copy = remaining > BUFF_SIZE ? BUFF_SIZE : remaining;
+	ft_memcpy(reader->buffer, reader->ctx.buffer.data
+		+ reader->ctx.buffer.cursor, to_copy);
+	reader->ctx.buffer.cursor += to_copy;
+	reader->index = 0;
+	reader->length = to_copy;
+	return (to_copy);
+}
+
+t_reader			create_reader_fd(int fd)
+{
+	return ((t_reader) {
+		.index = 0,
+		.length = 0,
+		.type = READER_TYPE_FD,
+		.ctx.fd.fd = fd,
+		.fill = reader_read_fd,
+	});
+}
+
+t_reader			create_reader_buffer(char *buffer, size_t buffer_length)
+{
+	return ((t_reader) {
+		.index = 0,
+		.length = 0,
+		.type = READER_TYPE_BUFFER,
+		.ctx.buffer.data = buffer,
+		.ctx.buffer.data_length = buffer_length,
+		.ctx.buffer.cursor = 0,
+		.fill = reader_read_buffer,
+	});
+}
+
+ssize_t				reader_read(t_reader *reader, char *dest, size_t length)
+{
+	size_t	copied;
+	size_t	remaining;
+	ssize_t	ret;
+
+	remaining = reader->length - reader->index;
+	copied = 0;
+	while (remaining < length)
+	{
+		copied += remaining;
+		ft_memcpy(dest, reader->buffer + reader->index, remaining);
+		if ((ret = reader->fill(reader)) <= 0)
+			return (ret < 0 ? ret : copied);
+		dest += remaining;
+		length -= remaining;
+		remaining = reader->length - reader->index;
+	}
+	ft_memcpy(dest, reader->buffer + reader->index, length);
+	reader->index += length;
+	return (length + copied);
 }
