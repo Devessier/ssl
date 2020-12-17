6e2c56e328868ed7dfe650249c3106ae0d57442c
Devessier <baptiste@devessier.fr>
2020-12-17 19:52:16 +0100

    fix: fix hexa printing when hex_length is equal to one
    Zero character was replaced in the while loop.

--- srcs/hexa.c
+++ srcs/hexa.c
@@ -6,37 +6,20 @@
 /*   By: bdevessi <baptiste@devessier.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/16 00:27:36 by bdevessi          #+#    #+#             */
-/*   Updated: 2020/12/16 01:07:16 by bdevessi         ###   ########.fr       */
+/*   Updated: 2020/12/17 20:02:49 by bdevessi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdint.h>
 #include <unistd.h>
 
-static int	hex_digits(uint8_t num)
-{
-	int		count;
-
-	if (num == 0)
-		return 1;
-	count = 0;
-	while (num != 0)
-	{
-		num >>= 4;
-		count++;
-	}
-	return count;
-}
-
 void		print_hexa_num(int fd, uint8_t num)
 {
 	const char	hexa_lookup_table[] = "0123456789abcdef";
 	char		buffer[2] = { 0 };
-	int			hex_length;
+	ssize_t		hex_length;
 
-	hex_length = hex_digits(num);
-	if (hex_length == 1)
-		buffer[0] = '0';
+	hex_length = 2;
 	while (--hex_length >= 0)
 	{
 		buffer[hex_length] = hexa_lookup_table[num & 0xf];
