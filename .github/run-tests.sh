4b9fb995ceabc5e8d2a33a579e679aa48425857b
Devessier <baptiste@devessier.fr>
2021-03-10 13:59:30 +0100

    chore: remove files that could not be accepted during the correction

--- .github/run-tests.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-make && ./test/shell/exec.sh
--- test/shell/exec.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-./test/shell/ssl.sh && ./test/shell/md5.sh && ./test/shell/sha256.sh && ./test/shell/sha224.sh && ./test/shell/sha512.sh && ./test/shell/sha384.sh && ./test/shell/sha512_224.sh && ./test/shell/sha512_256.sh
