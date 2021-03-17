2bba2408800ac8903b31c6e7a407276def114a3c
Devessier <baptiste@devessier.fr>
2021-03-17 10:59:33 +0100

    feat(writer): enable line breaking & use plain functions instead of t_writer fields

--- include/writer.h
+++ include/writer.h
@@ -6,7 +6,7 @@
 /*   By: bdevessi <baptiste@devessier.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 20:04:19 by bdevessi          #+#    #+#             */
-/*   Updated: 2021/03/17 01:35:06 by bdevessi         ###   ########.fr       */
+/*   Updated: 2021/03/17 10:35:11 by bdevessi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,23 @@ typedef struct			s_writer
 {
 	char			buffer[WRITER_BUFF_SIZE];
 	size_t			index;
+	size_t			bytes_written;
 	bool			activated;
 
 	int				fd;
-	void			(*write)(struct s_writer *writer, char *to_write, size_t length);
-	void			(*pad)(struct s_writer *writer, char pad_char, size_t count);
-	void			(*flush)(struct s_writer *writer);
-	void			(*close)(struct s_writer *writer);
+
+	char			*new_line;
+	size_t			new_line_length;
+	size_t			line_break;
 }						t_writer;
 
 t_writer				create_writer(int fd, char *filename);
+t_writer				create_writer_breaker(int fd, char *filename, size_t line_break, char *new_line);
 t_writer				create_writer_empty();
+
+void					writer_write(t_writer *writer, char *to_write, size_t length);
+void					writer_pad(t_writer *writer, char pad_char, size_t count);
+void					writer_flush(t_writer *writer);
+void					writer_close(t_writer *writer);
 
 #endif
--- srcs/writer.c
+++ srcs/writer.c
@@ -6,24 +6,25 @@
 /*   By: bdevessi <baptiste@devessier.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/16 20:04:13 by bdevessi          #+#    #+#             */
-/*   Updated: 2021/03/17 01:36:21 by bdevessi         ###   ########.fr       */
+/*   Updated: 2021/03/17 10:41:45 by bdevessi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "writer.h"
 
-static void		writer_write(t_writer *writer, char *to_write, size_t length)
+static void		writer_write_internal(t_writer *writer, char *to_write, size_t length)
 {
 	size_t	remaining;
 
 	if (writer->activated == false)
 		return ;
+	writer->bytes_written += length;
 	remaining = WRITER_BUFF_SIZE - writer->index;
 	while (remaining < length)
 	{
 		ft_memcpy(writer->buffer + writer->index, to_write, remaining);
-		writer->flush(writer);
+		writer_flush(writer);
 		to_write += remaining;
 		length -= remaining;
 		remaining = WRITER_BUFF_SIZE;
@@ -32,19 +33,42 @@ static void		writer_write(t_writer *writer, char *to_write, size_t length)
 	writer->index += length;
 }
 
-static void		writer_pad(t_writer *writer, char pad_char, size_t count)
+void			writer_write(t_writer *writer, char *to_write, size_t length)
+{
+	size_t	line_remainig;
+
+	if (writer->activated == false)
+		return ;
+	if (writer->line_break == 0)
+	{
+		writer_write_internal(writer, to_write, length);
+		return ;
+	}
+	line_remainig = writer->line_break - (writer->bytes_written % (writer->line_break + 1));
+	while (line_remainig < length)
+	{
+		writer_write_internal(writer, to_write, line_remainig);
+		writer_write_internal(writer, writer->new_line, writer->new_line_length);
+		to_write += line_remainig;
+		length -= line_remainig;
+		line_remainig = writer->line_break - (writer->bytes_written % (writer->line_break + 1));
+	}
+	writer_write_internal(writer, to_write, length);
+}
+
+void			writer_pad(t_writer *writer, char pad_char, size_t count)
 {
 	size_t	index;
 
 	index = 0;
 	while (index < count)
 	{
-		writer->write(writer, &pad_char, 1);
+		writer_write(writer, &pad_char, 1);
 		index++;
 	}
 }
 
-static void		writer_flush(t_writer *writer)
+void			writer_flush(t_writer *writer)
 {
 	if (writer->activated == false || writer->index == 0)
 		return ;
@@ -53,7 +77,7 @@ static void		writer_flush(t_writer *writer)
 	writer->index = 0;
 }
 
-static void		writer_close(t_writer *writer)
+void			writer_close(t_writer *writer)
 {
 	close(writer->fd);
 	writer->activated = false;
@@ -65,13 +89,22 @@ t_writer		create_writer(int fd, char *filename)
 
 	return ((t_writer){
 		.index = 0,
+		.bytes_written = 0,
 		.activated = true,
 		.fd = fd,
-		.write = writer_write,
-		.pad = writer_pad,
-		.flush = writer_flush,
-		.close = writer_close,
+		.line_break = 0,
 	});
+}
+
+t_writer				create_writer_breaker(int fd, char *filename, size_t line_break, char *new_line)
+{
+	t_writer	writer;
+
+	writer = create_writer(fd, filename);
+	writer.line_break = line_break;
+	writer.new_line = new_line;
+	writer.new_line_length = ft_strlen(new_line);
+	return (writer);
 }
 
 t_writer		create_writer_empty()
